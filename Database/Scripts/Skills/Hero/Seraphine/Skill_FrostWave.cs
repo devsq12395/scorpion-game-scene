0ec5138d83ccf5a02860aaa9cdc3a10440bdbcf1
devsq12395 <seasonquest.mail@gmail.com>
2024-01-09 22:51:39 +0800

    creating seraphine

---
+++ Database/Scripts/Skills/Hero/Seraphine/Skill_FrostWave.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_FrostWave : SkillTrig {
+
+    public string missileObj;
+    
+    public override void use_active (){
+        
+    }
+}
